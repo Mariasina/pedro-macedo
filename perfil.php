b7e993c9f4e80dc373c673361aaedd0abd658982
Emileeem <babaloogames@gmail.com>
2022-10-12 14:10:30 -0300

    Atualizando perfil
    deixando perfil bonitinho

--- perfil.php
+++ perfil.php
@@ -10,22 +10,75 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Painel do Administrador</title>
+        <link rel = "stylesheet" href = "css/perfil.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
-    <body>
-        
+<body>
+    <div class = "cabeça">
+        <nav class="bg-light border-bottom">
+            <div class="">
+                    Painel de Edição
+            </div>
+        </nav>
+    </div>
+    <div class="cor">
+    <header class = "py-2 mg-2 border-bottom">
+    <nav id = "menu-h">
+    <ul>
+
         <!--Mostra o nome do usuário cadastrado--> 
-        Olá, <?php echo $_SESSION['nome'];?><br>
+      <span class = fs-4> <b> Olá, <?php echo $_SESSION['nome'];?>!<br/> </b>
         <!--Todos os "if perfil 1" mostram informações que apenas o administrador vê."-->
         <?php if($_SESSION['perfil'] == 1){?>
-        Você é um Administrador.<br>
+      <b>  Você é um Administrador. </b> </span>
         <?php }?>
         <!--Links-->
-        <a href="index.html">Página inicial</a>
-        <a href="area_publicacao.php">Área de publicação</a>
-        <a href="usuarios_cadastrados.php">Usuários Cadastrados</a>
-        <?php if($_SESSION['perfil'] == 1){?>
-        <a href="aceitar_cadastro.php">Aceitar Cadastros Novos</a>
-        <?php }?>
-        <a href="bd/logout.php">Sair</a> 
-    </body>
+            <li>
+                <a href="index.html">Página inicial</a>
+            </li>
+            <li>
+                <a href="area_publicacao.php">Área de publicação</a>
+            </li>
+            <li>
+                <a href="usuarios_cadastrados.php">Usuários Cadastrados</a>
+            </li>
+            <?php if($_SESSION['perfil'] == 1){?>
+            <li>
+                <a href="aceitar_cadastro.php">Aceitar Cadastros Novos</a>
+            </li>
+            <?php }?>
+            <li>
+            <a href="bd/logout.php">Sair</a> 
+            </li>
+        </ul>
+    </nav>
+    </header> 
+    </div> <br/> <br/> <br/>
+    <div class="corpo"> 
+<main class="form-signin w-50 m-auto">
+  <form>
+    <h2 class="h1 mb-3 fw-normal">
+      Perfil
+    </h2>
+    <div class="form-floating">
+      Nome:  <?php echo $_SESSION['nome'];?> 
+      <input type="email" name="email" class="form-control w-100" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput" class="l1">
+        Email: <?php echo $_SESSION['email'];?>
+      </label>
+    </div>
+    <div class="form-floating">
+      <input type="password" name="senha" class="form-control w-100" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword" class="l1">
+        Senha:
+      </label>
+    </div><br>
+    <button class="w-50 btn btn-lg btn-primary" type="submit">
+      Editar
+    </button>
+  </form>
+</main>
+</div>
+</body>
 </html>
