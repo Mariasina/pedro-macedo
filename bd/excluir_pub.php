f6782c8761da83c9c67031ef187d00fa28cccde9
Maria Carolina <carolbeveer@gmail.com>
2022-10-19 08:43:27 -0300

    pequenas mudanças

--- bd/excluir_pub.php
+++ bd/excluir_pub.php
@@ -4,13 +4,14 @@
       
     if (isset($_POST['env'])){
     $cod = $_GET['cod'];
+    $codi = $cod;
 
-    $sql = "DELETE FROM publicacao WHERE cod = $cod ";
+    $sql = "DELETE FROM publicacao WHERE cod = $codi ";
 
     $resultado = mysqli_query($conexao, $sql);
 
     if($resultado == true){
         header("Location:../area_publicacao.php");
     }
-    
+}
 ?>
