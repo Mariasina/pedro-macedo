5213a02dbd1cd72c1adb2c5cda100a85b31d29ea
Maria Carolina <carolbeveer@gmail.com>
2022-11-25 11:12:49 -0300

    mudanças

--- bd/edit_pub.php
+++ bd/edit_pub.php
@@ -10,17 +10,87 @@
         $descricao = $_POST['descricao'];
         $codi = $cod;
         $img = $_FILES['imagem'];
+        $pagina = $_POST['paginas'];
 
         $uploaddir = '../img/uploads/';		
         $uploaddirN = 'img/uploads/';
+        
         if (isset($_FILES['imagem']) && !empty($_FILES['imagem']['name'])) {
         $criandoidimg = uniqid('img_').".".pathinfo($_FILES['imagem']['name'], PATHINFO_EXTENSION);
         $uploadfile = $uploaddir.$criandoidimg;
         $uploadfileN = $uploaddir.$criandoidimg;
         }
 
+        switch ($pagina) {
+            case "1": {
+              $sql = "INSERT INTO publicacao (pagina) VALUE '1'";
+              break;
+            }
+            case "2": { 
+                $sql = "INSERT INTO publicacao (pagina) VALUE '2'"; 
+              break;
+            }
+            case "3": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '3'";
+              break;
+            }
+            case "4": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '4'";
+              break;
+            }
+            case "5": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '5'";
+              break;
+            }
+            case "6": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '6'";
+              break;
+            }
+            case "7": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '7'";
+              break;
+            }
+            case "8": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '8'";
+              break;
+            }
+            case "9": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '9'";
+              break;
+            }
+            case "10": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '10'";
+              break;
+            }
+            case "11": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '11'";
+              break;
+            }
+            case "12": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '12'";
+              break;
+            }
+            case "13": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '13'";
+              break;
+            }
+            case "14": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '14'";
+              break;
+            }
+            case "15": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '15'";
+              break;
+            }
+            case "16": {
+                $sql = "INSERT INTO publicacao (pagina) VALUE '16'";
+              break;
+            }
+    
+          }
+
         //pegando as informações do bd
-        $sql = "UPDATE publicacao SET titulo = '$titulo', descricao = '$descricao' WHERE cod = '$codi'";
+        $sql = "UPDATE publicacao SET titulo = '$titulo', descricao = '$descricao', imagem = '$img', pagina = '$pagina' WHERE cod = '$codi'";
 
         //variável para concluir a edição
         $resultado = mysqli_query($conexao, $sql);
--- editar_publicacao.php
+++ editar_publicacao.php
@@ -41,15 +41,37 @@
 
             <label for="formFile" class="form-label"></label>
         <h3 class = "texto">Adicione uma imagem para alteração:</h3>
-            <input class="form-control" type="file" name="imagem" id="formFile"> <br> <br>
+            <input class="form-control" type="file" name="imagem" id="formFile" value="<?php echo $publicacao['imagem'];?>"> <br> <br>
         </div>
 
      <!-- DESCRICAO  -->
 
      <h3 class = "texto"> Adicione uma descrição para alteração.</h3>
         <textarea class = "form-control form-control-lg" name="descricao" rows="5" cols = "150" placeholder="Descrição...">
-            <?php echo $publicacao['descricao'];?>
+            <?php echo $publicacao['descricao'];
+            $selected = $publicacao['pagina'];
+            ?>
         </textarea><br><br/>
+
+        <div>Selecione a página onde a publicação irá aparecer:</div>
+            <select name="paginas">
+                <option value="1" <?php echo($selected == "1" ? "selected" : "" );?>> <a>Notícias</a></option>
+                <option value="2" <?php echo($selected == "2" ? "selected" : "" );?>><a>Fundamental</a></option>
+                <option value="3" <?php echo($selected == "3" ? "selected" : "" );?>><a>Médio</a></option>
+                <option value="4" <?php echo($selected == "4" ? "selected" : "" );?>><a>Profissionalizante</a></option>
+                <option value="5" <?php echo($selected == "5" ? "selected" : "" );?>><a>CELEM</a></option>
+                <option value="6" <?php echo($selected == "6" ? "selected" : "" );?>><a>Direção</a></option>
+                <option value="7" <?php echo($selected == "7" ? "selected" : "" );?>><a>Secretaria</a></option>
+                <option value="8" <?php echo($selected == "8" ? "selected" : "" );?>><a>Equipe Multidisciplinar</a></option>
+                <option value="9" <?php echo($selected == "9" ? "selected" : "" );?>><a>Espaço do Professor</a></option>
+                <option value="10" <?php echo($selected == "10" ? "selected" : "" );?>><a>Colégio</a></option>  
+                <option value="11" <?php echo($selected == "11" ? "selected" : "" );?>><a>Professores</a></option>
+                <option value="12" <?php echo($selected == "12" ? "selected" : "" );?>><a>Eventos</a></option> 
+                <option value="13" <?php echo($selected == "13" ? "selected" : "" );?>><a>Cursos</a></option>
+                <option value="14" <?php echo($selected == "14" ? "selected" : "" );?>><a>Calendario 2022</a></option>
+                <option value="15" <?php echo($selected == "15" ? "selected" : "" );?>><a>Material para 6º anos</a></option>
+                <option value="16" <?php echo($selected == "16" ? "selected" : "" );?>><a>Biblioteca CPM</a></option>
+            </select><br><br>
 
     <!-- ENVIAR  -->
 
